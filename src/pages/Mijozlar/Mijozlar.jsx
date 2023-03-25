29bc0bc64c469b68fd6a92eb0bf81913b6b7e4f7
sadriddinoov <sadriddinovbilol941gmail.com>
2023-03-25 20:14:19 +0500

    pages

--- src/pages/Mijozlar/Mijozlar.jsx
+++ src/pages/Mijozlar/Mijozlar.jsx
@@ -1,13 +1,15 @@
-import './Mahsulotlar.scss'
+import './Mijozlar.scss'
+
 
 import Header from   '../../container/header/header'
 import Sidebar from   '../../container/sidebar/sidebar'
 
-export default function Mahsulotar(){
+export default function Mijozlar(){
     return (
         <>
         <Header/>
         <Sidebar/>
+
         <div className="wraper">
             
         </div>
