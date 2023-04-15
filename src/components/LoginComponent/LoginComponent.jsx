1bd1665f8bbf470d52ac89d433c0888c9ed31d75
sadriddinoov <106529773+sadriddinoov@users.noreply.github.com>
2023-04-15 18:04:05 +0500

    Update LoginComponent.jsx

--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -3,43 +3,7 @@ import '../../assets/settings/settings.scss'
 import logo from '../../assets/images/logo.svg'
 import './LoginComponent.scss'
 
-const LoginComponent = () => {
-  let username = document.querySelector('.login__form-login')
-  let password = document.querySelector('.login__form-password')
-  const form = document.querySelector('.login__form')
-  
-  
-  
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    username = username.value.trim()
-    password = password.value.trim()
-    
-    const value = JSON.stringify({
-      "user_name": username,
-      "user_password": password,
-      "user_role" :  'admin'
-    })
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: value,
-    };
-    
-    fetch(`http://localhost:3000/users`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      if (result.id) {
-        alert('succes')
-      } else {
-        alert('invalid request')
-      }
-    })
-    .catch(error => console.log('error', error));
-  })
+
   
   return (
     <section className="login">
